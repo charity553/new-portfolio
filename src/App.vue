8460cc0e9ef846c408cc1567149611a6673194f2
Charity Mulei <kokicharity3@gmail.com>
2025-06-30 16:49:44 +0300

    Added utiities and start of Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
         <Navbar />
+        <HeroSection />
     </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent } from 'vue';
 const Navbar = defineAsyncComponent(() => import('@/components/layout/Navbar.vue'));
+const HeroSection = defineAsyncComponent(() => import('@/components/layout/HeroSection.vue'));
 </script>
