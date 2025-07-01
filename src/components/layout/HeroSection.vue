65f3da1ad553b85dd447dc0f8c9936fb022f6324
Charity Mulei <kokicharity3@gmail.com>
2025-07-01 13:04:30 +0300

    Counter Section

--- src/components/layout/HeroSection.vue
+++ src/components/layout/HeroSection.vue
@@ -8,6 +8,7 @@
         <header class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc]
         via-[#785ae4] to-secondary opacity-30 dark:opacity-20 blur-[100px] right-10 bottom-0 hidden md:block">
         </header>
+        
         <main class="absolute grid lg:grid-cols-2 grid-cols-1 items-center p-2 lg:p-8">
             <header class="text-center lg:text-left">
                 <h1 class="mb-6 text-4xl md:text=5xl lg:text-6xl font-bold">
